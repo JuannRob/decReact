3ecf59182eaa573d759aae9881b8a2e7719e9a87
Juan Robledo <juannrob@gmail.com>
2023-07-03 23:17:09 -0300

    header positioning

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <div className="bg-main-color">
-        <h1 className="text-5xl text-zinc-50">DecretApp</h1>
+      <div className="w-100 h-36 bg-main-color">
+        <h1 className="block text-center text-5xl text-zinc-50">DecretApp</h1>
       </div>
     </header>
   );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Search = () => {
-  return <div className="inline bg-main-color text-white">Hola mundo!</div>;
+  return <div className="inline h-80 w-1/2 bg-main-color text-white">Coso</div>;
 };
 
 export default Search;
